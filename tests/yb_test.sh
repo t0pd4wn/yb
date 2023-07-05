95f27e78a7258621eacb36fc6d1e9c50beacd93d
t0pd4wn <t0pd4wn@protonmail.com>
2023-07-05 20:51:43 +0200

    add: support for keys with spaces; added some parsing tests

--- tests/yb_test.sh
+++ tests/yb_test.sh
@@ -25,61 +25,75 @@ tests(){
 	parse=$(./yb -f tests/yb.yaml -k "y")
 	check_test "b" "${parse}"
 
-	echo -e "\U1F4AC Test 1.4: parse file with key chain and raw option"
+	echo -e "\U1F4AC Test 1.4: parse file with key chain"
+	parse=$(./yb -f tests/yb.yaml -k "yb.yaml.bash")
+	check_test "- IFS
+- BASED
+- PARSER" "${parse}"
+
+	echo -e "\U1F4AC Test 1.5: parse file with key chain, targetting an inline key"
+	parse=$(./yb -f tests/yb.yaml -k "yb.yaml.bash.IFS")
+	check_test "BASED PARSER" "${parse}"
+
+	echo -e "\U1F4AC Test 1.6: parse file with key chain, targetting a key with a space"
+	parse=$(./yb -f tests/yb.yaml -k "complex.strings.I\am")
+	check_test "a complex string" "${parse}"
+
+	echo -e "\U1F4AC Test 1.7: parse file with key chain and raw option"
 	parse=$(./yb -Rf tests/yb.yaml -k "yb.yaml.bash")
 	check_test "- IFS
 - BASED
 - PARSER" "${parse}"
 
-	echo -e "\U1F4AC Test 1.5: parse file with a number output"
+	echo -e "\U1F4AC Test 1.8: parse file with a number output"
 	parse=$(./yb -Rf tests/yb.yaml -k "yaml.yaml.yaml.bash.IFS.BASED.number")
 	check_test "101" "${parse}"
 
-	echo -e "\U1F4AC Test 1.6: parse file with a boolean output"
+	echo -e "\U1F4AC Test 1.9: parse file with a boolean output"
 	parse=$(./yb -Af tests/yb.yaml -k "do.exist")
 	check_test "true" "${parse}"
 
-  echo -e "\U1F4AC Test 1.7: parse file with raw option and depth option"
+  echo -e "\U1F4AC Test 1.10: parse file with raw option and depth option"
 	parse=$(./yb -Rdf tests/yb.yaml -k "yb.yaml.bash")
 	check_test "      - IFS
       - BASED
       - PARSER" "${parse}"
 
-	echo -e "\U1F4AC Test 1.8: parse file with key chain and formatting"
+	echo -e "\U1F4AC Test 1.11: parse file with key chain and formatting"
 	parse=$(./yb -Ff tests/yb.yaml -k "yb.yaml.bash")
 	check_test ".bash_- IFS
 .bash_- BASED
 .bash_- PARSER" "${parse}"
 
-  echo -e "\U1F4AC Test 1.9: parse file with formatting and depth"
+  echo -e "\U1F4AC Test 1.12: parse file with formatting and depth"
   parse=$(./yb -Fdf tests/yb.yaml -k "yb.yaml.bash")
   check_test "....bash_- IFS
 ....bash_- BASED
 ....bash_- PARSER" "${parse}"
 
-  echo -e "\U1F4AC Test 1.10: parse file with key chain and array option"
+  echo -e "\U1F4AC Test 1.13: parse file with key chain and array option"
   parse=$(./yb -Af tests/yb.yaml -k "yb.yaml.bash")
   check_test "IFS BASED PARSER" "${parse}"
 
-  echo -e "\U1F4AC Test 1.11: parse file with key chain and array option"
+  echo -e "\U1F4AC Test 1.14: parse file with key chain format and array options"
 	parse=$(./yb -FAf tests/yb.yaml -k "yb.yaml.bash")
 	check_test ".bash_IFS .bash_BASED .bash_PARSER" "${parse}"
 
-  echo -e "\U1F4AC Test 1.12: parse file with formatting, array and depth"
+  echo -e "\U1F4AC Test 1.15: parse file with formatting, array and depth"
   parse=$(./yb -FAdf tests/yb.yaml -k "yb.yaml.bash")
   check_test "....bash_IFS ....bash_BASED ....bash_PARSER" "${parse}"
 
-	echo -e "\U1F4AC Test 1.13: parse file with all -lLn outer options"
+	echo -e "\U1F4AC Test 1.16: parse file with all -lLn outer options"
 	parse=$(./yb -RlLnf tests/yb.yaml -k "yb.yaml.bash")
 	check_test "- IFS{{line}}{{4}}{{6}}
 - BASED{{line}}{{4}}{{7}}
 - PARSER{{line}}{{4}}{{8}}" "${parse}"
 
-  echo -e "\U1F4AC Test 1.14: parse file with all options but depth"
+  echo -e "\U1F4AC Test 1.17: parse file with all options but depth"
   parse=$(./yb -FARlLnf tests/yb.yaml -k "yb.yaml.bash")
   check_test ".bash_IFS{{line}}{{4}}{{6}} .bash_BASED{{line}}{{4}}{{7}} .bash_PARSER{{line}}{{4}}{{8}}" "${parse}"
         
-  echo -e "\U1F4AC Test 1.15: parse file with all compatible options"
+  echo -e "\U1F4AC Test 1.18: parse file with all compatible options"
   parse=$(./yb -FARldLnf tests/yb.yaml -k "yb.yaml.bash")
   check_test "....bash_IFS{{line}}{{4}}{{6}} ....bash_BASED{{line}}{{4}}{{7}} ....bash_PARSER{{line}}{{4}}{{8}}" "${parse}"
 
@@ -111,7 +125,7 @@ tests(){
   # clean yaml file
   sed -i '38,39d' tests/yb.yaml
   sed -i '42,43d' tests/yb.yaml
-  sed -i '47,48d' tests/yb.yaml
+  sed -i '45,46d' tests/yb.yaml
 
   # end message
   echo ""
